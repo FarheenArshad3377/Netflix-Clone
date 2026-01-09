c219de9ec340ed1bcca513064afc90da3f7a11e9
FarheenArshad3377 <farheenarshad2233@gmail.com>
2026-01-10 04:58:31 +0500

    Update changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import "./index.css"
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import "./index.css";
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import SignUp from './components/sign.jsx';
 import Login from './components/login.jsx';
 import Home from './components/home.jsx';
 import Movie from './components/Movie.jsx';
-const router = createBrowserRouter([
+
+// Create router
+const router = createBrowserRouter(
+  [
+    {
+      path: "/", // root
+      element: <App />,
+      children: [
+        { index: true, element: <Home /> }, // default page
+        { path: "signup", element: <SignUp /> },
+        { path: "login", element: <Login /> },
+        { path: "movie", element: <Movie /> },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    children: [
-      { index: true, element: <Home /> }, // default page
-      { path: "signup", element: <SignUp /> },
-      { path: "login", element: <Login /> },
-      { path: "movie", element: <Movie /> },
-    ],
-  },
-]);
+    basename: "/Netflix-Clone", // 👈 GitHub repo name
+  }
+);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <RouterProvider router={router} />,
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
