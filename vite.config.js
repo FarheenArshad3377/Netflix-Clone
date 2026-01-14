ad3b2279d984da14010a3c9d13fd7b07a8782724
FarheenArshad3377 <farheenarshad2233@gmail.com>
2026-01-14 15:34:38 +0500

    Fix base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: "/Netflix-Clone/" // 👈 Ye line add kar do
+    base: "/" // 👈 Ye line add kar do
 
 })
